5c366560222dd056e0ba579f110afcb3d561480a
Maria Vorslova <mariavorslova@gmail.com>
2023-01-23 01:39:49 +0100

    main fn basic structure and readline implemented

--- src/minishell.c
+++ src/minishell.c
@@ -6,11 +6,23 @@
 /*   By: mvorslov <mvorslov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 22:28:07 by ede-smet          #+#    #+#             */
-/*   Updated: 2023/01/22 17:35:51 by mvorslov         ###   ########.fr       */
+/*   Updated: 2023/01/23 01:30:51 by mvorslov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/mini_fun.h"
+
+int	ft_execute(char *line)
+{
+	(void)line;
+	return(0);
+}
+
+int	ft_parse(char *line)
+{
+	(void)line;
+	return (0);
+}
 
 int	main(void)
 {
@@ -19,6 +31,12 @@ int	main(void)
 	while (1)
 	{
 		line = readline("minishell $");
+		if (ft_strlen(line) != 0)
+		{
+			add_history(line);
+			ft_parse(line);
+			ft_execute(line);
+		}
 		free(line);
 	}
 	return (0);
