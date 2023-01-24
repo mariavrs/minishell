6211818ea4865362870861a5d27ab71de4822b33
Maria Vorslova <mariavorslova@gmail.com>
2023-01-24 01:17:53 +0100

    minor formatting of shell name and ft_parse started

--- src/minishell.c
+++ src/minishell.c
@@ -6,36 +6,38 @@
 /*   By: mvorslov <mvorslov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 22:28:07 by ede-smet          #+#    #+#             */
-/*   Updated: 2023/01/23 01:30:51 by mvorslov         ###   ########.fr       */
+/*   Updated: 2023/01/24 01:15:50 by mvorslov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/mini_fun.h"
 
-int	ft_execute(char *line)
+int	ft_exec_tree(t_cmd *cmd)
 {
-	(void)line;
-	return(0);
+	(void)cmd;
+	return (0);
 }
 
-int	ft_parse(char *line)
+t_cmd	*ft_parse(char *line)
 {
-	(void)line;
-	return (0);
+	t_cmd	*ret;
+	char	*eline;
+
+	eline = line + ft_strlen(line);
+	return (ret);
 }
 
 int	main(void)
 {
-	char *line;
+	char	*line;
 
 	while (1)
 	{
-		line = readline("minishell $");
+		line = readline("\033[1;36mminishell>>> \033[0m");
 		if (ft_strlen(line) != 0)
 		{
 			add_history(line);
-			ft_parse(line);
-			ft_execute(line);
+			ft_exec_tree(ft_parse(line));
 		}
 		free(line);
 	}
