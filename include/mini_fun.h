c595a100419d0de480773ccd1f34b7c519f65ff7
Maria Vorslova <mariavorslova@gmail.com>
2023-01-24 21:27:21 +0100

    parser and runner prototypes added

--- include/mini_fun.h
+++ include/mini_fun.h
@@ -6,7 +6,7 @@
 /*   By: mvorslov <mvorslov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 23:40:48 by ede-smet          #+#    #+#             */
-/*   Updated: 2023/01/22 18:05:09 by mvorslov         ###   ########.fr       */
+/*   Updated: 2023/01/24 21:26:20 by mvorslov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,23 @@ void	ft_export(void);
 void	ft_unset(void);
 void	ft_env(void);
 void	ft_exit(void);
+
+// Minishell
+t_cmd	*ft_parse(char *line);
+t_cmd	*parse_exec(char *line, char *eline);
+t_cmd	*parse_redir(char *line, char *eline);
+t_cmd	*parse_pipe(char *line, char *eline);
+t_cmd	*parse_list(char *line, char *eline);
+
+int		list_delim_locator(char *line, char *eline, char **del, char **edel);
+int		words_counter(char *line, char *eline);
+void	trim_whitespaces(char **line, char **eline);
+int		check_whitespace(char c);
+
+void	ft_exec_tree(t_cmd *cmd, int exit_status);
+void	run_exec(t_exec *cmd, int exit_status);
+void	run_redir(t_redir *cmd, int exit_status);
+void	run_pipe(t_pipe *cmd, int exit_status);
+void	run_list(t_list *cmd, int exit_status);
 
 #endif
