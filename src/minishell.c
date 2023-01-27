a52f578a7fa4b2b4927c17bddf1cd6613fe2bbb3
Erick DE SMET <erickdesmet@gmail.com>
2023-01-27 14:27:27 +0100

    test of pwd and cd added

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: ede-smet <ede-smet@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 22:28:07 by ede-smet          #+#    #+#             */
-/*   Updated: 2023/01/26 20:49:23 by ede-smet         ###   ########.fr       */
+/*   Updated: 2023/01/27 14:16:06 by ede-smet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,10 @@ int	main(int argc, char *argv[])
 	printf("############ Calling of echo ############\n");
 	ft_echo(argv);
 	printf("############ Calling of pwd ############\n");
+	ft_pwd();
+	printf("############ Calling of cd ############\n");
+	if (ft_cd(argv) == 1)
+		printf("J'encule des mouches !\n");
 	ft_pwd();
 	return (0);
 }
