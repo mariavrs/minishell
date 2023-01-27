b9e10213e6f5cf5166dbf9a4fb74964b39b94f23
Erick DE SMET <erickdesmet@gmail.com>
2023-01-27 14:25:27 +0100

    buitin cd done, I used chdir function

--- src/builtins/ft_cd.c
+++ src/builtins/ft_cd.c
@@ -6,14 +6,15 @@
 /*   By: ede-smet <ede-smet@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 16:54:40 by ede-smet          #+#    #+#             */
-/*   Updated: 2023/01/26 20:24:42 by ede-smet         ###   ########.fr       */
+/*   Updated: 2023/01/27 14:14:57 by ede-smet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/mini_fun.h"
 
-int	ft_cd(void)
+int	ft_cd(char **input)
 {
-	printf("Hey! Im Cd, nice to meet you ! (>.o)\n");
-	return (0);
+	if (chdir(input[1]) == 0)
+		return (0);
+	return (1);
 }
