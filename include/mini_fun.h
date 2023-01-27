717d2d14f647c698d1096703ab5765230d1f7be2
Erick DE SMET <erickdesmet@gmail.com>
2023-01-27 14:26:30 +0100

    Prototypes of ft_cd and ft_pwd modified

--- include/mini_fun.h
+++ include/mini_fun.h
@@ -6,7 +6,7 @@
 /*   By: ede-smet <ede-smet@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 23:40:48 by ede-smet          #+#    #+#             */
-/*   Updated: 2023/01/26 20:24:19 by ede-smet         ###   ########.fr       */
+/*   Updated: 2023/01/27 13:59:54 by ede-smet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 # define MINI_FUN_H
 # include "../lib/libft/libft.h"
 # include "mini_struct.h"
+# include <sys/types.h>
+# include <dirent.h>
 
 // Builtins prototypes
 int	ft_echo(char *input[]);
-int	ft_cd(void);
+int	ft_cd(char **input);
 int	ft_pwd(void);
 int	ft_export(void);
 int	ft_unset(void);
