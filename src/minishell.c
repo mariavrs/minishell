378ef1ee89b18fdc38a51dfba2238e342f43012f
DE SMET Erick <erickdesmet@gmail.com>
2023-01-30 21:18:15 +0100

    custom minishell modified for env tests

--- src/minishell.c
+++ src/minishell.c
@@ -6,15 +6,18 @@
 /*   By: ede-smet <ede-smet@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 22:28:07 by ede-smet          #+#    #+#             */
-/*   Updated: 2023/01/27 14:16:06 by ede-smet         ###   ########.fr       */
+/*   Updated: 2023/01/30 21:16:24 by ede-smet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/mini_fun.h"
 
-int	main(int argc, char *argv[])
+int	main(int argc, char *argv[], char *envp[])
 {
+	char	**env;
+
 	(void)argc;
+	env = ft_parent_env_cpy(envp);
 	printf("############ Calling of echo ############\n");
 	ft_echo(argv);
 	printf("############ Calling of pwd ############\n");
@@ -23,5 +26,7 @@ int	main(int argc, char *argv[])
 	if (ft_cd(argv) == 1)
 		printf("J'encule des mouches !\n");
 	ft_pwd();
+	printf("############ Calling of env ############\n");
+	ft_env(env);
 	return (0);
 }
