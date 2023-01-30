dbbb51d9faa8b545a0428cffc769b7fb5813d0df
DE SMET Erick <erickdesmet@gmail.com>
2023-01-30 21:18:47 +0100

    prototypes reorganised

--- include/mini_fun.h
+++ include/mini_fun.h
@@ -6,7 +6,7 @@
 /*   By: ede-smet <ede-smet@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 23:40:48 by ede-smet          #+#    #+#             */
-/*   Updated: 2023/01/27 13:59:54 by ede-smet         ###   ########.fr       */
+/*   Updated: 2023/01/30 21:16:40 by ede-smet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,14 @@
 # include <dirent.h>
 
 // Builtins prototypes
-int	ft_echo(char *input[]);
-int	ft_cd(char **input);
-int	ft_pwd(void);
-int	ft_export(void);
-int	ft_unset(void);
-int	ft_env(void);
-int	ft_exit(void);
+int		ft_echo(char *input[]);
+int		ft_cd(char **input);
+int		ft_pwd(void);
+int		ft_export(void);
+int		ft_unset(void);
+int		ft_exit(void);
+// Environment functions
+int		ft_env(char **env);
+char	**ft_parent_env_cpy(char **envp);
 
 #endif
