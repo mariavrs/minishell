35e8a48417606f8654ff364f4f0469c0a436a5cf
DE SMET Erick <erickdesmet@gmail.com>
2023-01-30 21:19:23 +0100

    ft_env modified, v0.2

--- src/builtins/ft_env.c
+++ src/builtins/ft_env.c
@@ -6,14 +6,49 @@
 /*   By: ede-smet <ede-smet@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 16:54:40 by ede-smet          #+#    #+#             */
-/*   Updated: 2023/01/26 20:24:53 by ede-smet         ###   ########.fr       */
+/*   Updated: 2023/01/30 21:17:01 by ede-smet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/mini_fun.h"
 
-int	ft_env(void)
+static int	ft_count_elem(char **envp)
 {
-	printf("Hey! Im Env, nice to meet you ! (>.o)\n");
+	int	i;
+
+	i = 0;
+	while (*envp++)
+		i++;
+	return (i);
+}
+
+char	**ft_parent_env_cpy(char **envp)
+{
+	int		nb_env;
+	char	**env;
+	int		i;
+
+	i = -1;
+	env = NULL;
+	nb_env = ft_count_elem(envp);
+	env = malloc((nb_env + 1) * sizeof(char *));
+	if (!env)
+		return (NULL);
+	while (envp[++i])
+	{
+		env[i] = malloc ((ft_strlen(envp[i])) * sizeof(char));
+		ft_strlcpy(env[i], envp[i], ft_strlen(envp[i]) + 1);
+	}
+	env[++i] = "\0";
+	return (env);
+}
+
+int	ft_env(char **env)
+{
+	int	i;
+
+	i = -1;
+	while (env[++i])
+		printf("%s\n", env[i]);
 	return (0);
 }
