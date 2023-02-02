c20ea10c1dccb1b8d00910ca789e9c576f7aa283
Ubuntu <perso@42.fr>
2023-02-02 13:47:07 +0000

    leaks fixed

--- src/builtins/ft_cd.c
+++ src/builtins/ft_cd.c
@@ -6,25 +6,27 @@
 /*   By: mvorslov <mvorslov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 16:54:40 by ede-smet          #+#    #+#             */
-/*   Updated: 2023/02/01 14:18:19 by mvorslov         ###   ########.fr       */
+/*   Updated: 2023/02/02 13:33:46 by mvorslov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/mini_fun.h"
 
-int	ft_cd(char **input, t_env *env)
+int	ft_cd(char **input, char ***env)
 {
 	char	tmp_dir[1000];
+	char	*home;
 
-	if (!input || !input[1])
-		return (1);
 	getcwd(tmp_dir, 1000);
 	env_edit(env, "OLDPWD", tmp_dir);
+	home = env_get(*env, "HOME");
+	if ((!input || !input[1]) && chdir(home) == 0)
+		return (env_edit(env, "PWD", home), free(home), 0);
 	if (chdir(input[1]) == 0)
 	{
 		getcwd(tmp_dir, 1000);
 		env_edit(env, "PWD", tmp_dir);
-		return (0);
+		return (free(home), 0);
 	}
-	return (1);
+	return (free(home), 1);
 }
