b5c9365a6007d6be29a505c580ea3190f3e696e0
Ubuntu <perso@42.fr>
2023-02-02 13:45:11 +0000

    env functions added and some prototypes modified

--- include/mini_fun.h
+++ include/mini_fun.h
@@ -6,7 +6,7 @@
 /*   By: mvorslov <mvorslov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 23:40:48 by ede-smet          #+#    #+#             */
-/*   Updated: 2023/02/01 13:53:02 by mvorslov         ###   ########.fr       */
+/*   Updated: 2023/02/02 13:29:38 by mvorslov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,19 @@
 
 // Builtins prototypes
 int		ft_echo(char *input[]);
-int		ft_cd(char **input, t_env *env);
+int		ft_cd(char **input, char ***env);
 int		ft_pwd(char **env);
 int		ft_export(void);
 int		ft_unset(void);
 int		ft_exit(void);
-// Environment functions
 int		ft_env(char **env);
-void	ft_parent_env_cpy(t_env *env, char **envp);
-int		env_edit(t_env *env, char *var, char *value);
+// Environment extra functions
+void	ft_fill_env(char *env, char *var, char *value);
+int		ft_count_elem(char **envp);
+int	ft_parent_env_cpy(char ***env, char **envp);
+int		env_edit(char ***env, char *var, char *value);
 char	*env_get(char **env, char *var);
+int		env_add(char ***env, char *value);
 
 // Generic functions
 void	free_table(char **table);
--- include/mini_struct.h
+++ include/mini_struct.h
@@ -6,17 +6,11 @@
 /*   By: mvorslov <mvorslov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 00:38:32 by ede-smet          #+#    #+#             */
-/*   Updated: 2023/02/01 13:09:48 by mvorslov         ###   ########.fr       */
+/*   Updated: 2023/02/02 10:45:01 by mvorslov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINI_STRUCT_H
 # define MINI_STRUCT_H
-
-
-typedef struct  s_env
-{
-    char    **env_cp;
-}   t_env;
 
 #endif
