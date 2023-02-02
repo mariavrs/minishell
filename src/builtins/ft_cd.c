7880044fd2f8e20393c9fa0cd8484a41b09c6748
Ubuntu <perso@42.fr>
2023-02-02 10:30:15 +0000

    ft_cd with env

--- src/builtins/ft_cd.c
+++ src/builtins/ft_cd.c
@@ -3,18 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   ft_cd.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ede-smet <ede-smet@42.fr>                  +#+  +:+       +#+        */
+/*   By: mvorslov <mvorslov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 16:54:40 by ede-smet          #+#    #+#             */
-/*   Updated: 2023/01/27 14:14:57 by ede-smet         ###   ########.fr       */
+/*   Updated: 2023/02/01 14:18:19 by mvorslov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/mini_fun.h"
 
-int	ft_cd(char **input)
+int	ft_cd(char **input, t_env *env)
 {
+	char	tmp_dir[1000];
+
+	if (!input || !input[1])
+		return (1);
+	getcwd(tmp_dir, 1000);
+	env_edit(env, "OLDPWD", tmp_dir);
 	if (chdir(input[1]) == 0)
+	{
+		getcwd(tmp_dir, 1000);
+		env_edit(env, "PWD", tmp_dir);
 		return (0);
+	}
 	return (1);
 }
