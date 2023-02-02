f87c376dcaf70d561162317a064bfdad5c046eec
Ubuntu <perso@42.fr>
2023-02-02 13:47:50 +0000

    updated with env_add behavior

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: mvorslov <mvorslov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 22:28:07 by ede-smet          #+#    #+#             */
-/*   Updated: 2023/02/02 10:28:43 by mvorslov         ###   ########.fr       */
+/*   Updated: 2023/02/02 12:43:51 by mvorslov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,22 @@
 
 int	main(int argc, char *argv[], char *envp[])
 {
-	t_env	env;
+	char	**env;
 
 	(void)argc;
 	ft_parent_env_cpy(&env, envp);
 	printf("############ Calling of echo ############\n");
 	ft_echo(argv);
 	printf("############ Calling of pwd ############\n");
-	ft_pwd(env.env_cp);
+	ft_pwd(env);
 	printf("############ Calling of cd ############\n");
 	if (ft_cd(argv, &env) == 1)
 		printf("Path invalid !\n");
-	ft_pwd(env.env_cp);
+	ft_pwd(env);
 	printf("############ Calling of env ############\n");
-	ft_env(env.env_cp);
-	free_table(env.env_cp);
-	return (0);
+	ft_env(env);
+	printf("############ add var in env ############\n");
+	env_add(&env, "NTM=lol");
+	ft_env(env);
+	return (free_table(env), 0);
 }
