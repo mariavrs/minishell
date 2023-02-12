118a63cc4ec297106ec5ca7249a69c3f2a6a3e47
DE SMET Erick <erickdesmet@gmail.com>
2023-02-12 20:34:10 +0100

    new function to check if the env variable exist or not

--- include/mini_fun.h
+++ include/mini_fun.h
@@ -6,7 +6,7 @@
 /*   By: ede-smet <ede-smet@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 23:40:48 by ede-smet          #+#    #+#             */
-/*   Updated: 2023/02/12 19:52:46 by ede-smet         ###   ########.fr       */
+/*   Updated: 2023/02/12 20:32:18 by ede-smet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ int		ft_unset(void);
 int		ft_exit(void);
 int		ft_env(char **env);
 // Environment extra functions
+int		env_exist(char **env, char *var);
 void	ft_fill_env(char *env, char *var, char *value);
 int		ft_count_elem(char **envp);
 int		ft_parent_env_cpy(char ***env, char **envp);
