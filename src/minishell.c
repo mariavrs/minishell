4e2621dfb0ceb25717edd01a6f82f74f9f09f6da
DE SMET Erick <erickdesmet@gmail.com>
2023-02-23 16:38:49 +0100

    last minishell perso

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: ede-smet <ede-smet@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 22:28:07 by ede-smet          #+#    #+#             */
-/*   Updated: 2023/02/23 15:17:32 by ede-smet         ###   ########.fr       */
+/*   Updated: 2023/02/23 16:28:11 by ede-smet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,36 @@
 
 int	main(int argc, char *argv[], char *envp[])
 {
-	char	**env;
+	t_msh	msh;
 
 	(void)argc;
-	ft_parent_env_cpy(&env, envp);
+	ft_parent_env_cpy(&(msh.envp), envp);
 	printf("############ Calling of echo ############\n");
 	ft_echo(argv);
 	printf("############ Calling of pwd ############\n");
 	ft_pwd();
 	printf("############ Calling of env ############\n");
-	ft_env(env);
+	ft_env(msh);
 	printf("############ Calling of cd ############\n");
-	env_del(&env, "PWD");
-	env_del(&env, "PWD");
-	env_del(&env, "OLDPWD");
-	env_del(&env, "HOME");
-	ft_cd(argv, &env);
+	env_del(&(msh.envp), "PWD");
+	env_del(&(msh.envp), "PWD");
+	env_del(&(msh.envp), "OLDPWD");
+	env_del(&(msh.envp), "HOME");
+	ft_cd(argv, &(msh.envp));
 	ft_pwd();
 	printf("############ Calling of env ############\n");
-	ft_env(env);
+	ft_env(msh);
 	printf("############ add NTM in env ############\n");
-	env_add(&env, "NTM=lol");
-	ft_env(env);
-	printf("############ del TERM from env ############\n");
-	env_del(&env, "TERM");
-	ft_env(env);
+	env_add(&msh.envp, "NTM=lol");
+	ft_env(msh);
+	printf("############ del LOGNAME from env ############\n");
+	env_del(&(msh.envp), "LOGNAME");
+	ft_env(msh);
 	printf("############ Calling of export ############\n");
-	ft_unset(&env, argv);
-	ft_env(env);
+	ft_export(&msh, argv);
+	ft_env(msh);
 	printf("############ Calling of unset ############\n");
-	ft_unset(&env, argv);
-	ft_env(env);
-	return (free_table(env), 0);
+	ft_unset(&msh, argv);
+	ft_env(msh);
+	return (free_table(msh.envp), 0);
 }
