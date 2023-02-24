3d0ced636f8ac6c4c32ed867fc10626f0fee8cd0
DE SMET Erick <erickdesmet@gmail.com>
2023-02-24 17:41:26 +0100

    ft_cd works now with the struct msh

--- src/builtins/ft_cd.c
+++ src/builtins/ft_cd.c
@@ -6,28 +6,28 @@
 /*   By: ede-smet <ede-smet@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 16:54:40 by ede-smet          #+#    #+#             */
-/*   Updated: 2023/02/12 19:58:53 by ede-smet         ###   ########.fr       */
+/*   Updated: 2023/02/23 17:03:22 by ede-smet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/mini_fun.h"
 
-int	ft_cd(char **input, char ***env)
+int	ft_cd(char **input, t_msh *msh)
 {
 	char	current_dir[PATH_MAX];
 	char	*home;
 
 	getcwd(current_dir, PATH_MAX);
-	home = env_get(*env, "HOME");
+	home = env_get(msh->envp, "HOME");
 	if (!home && !input[1])
 		return (free(home), write(2, "cd: HOME not set\n", 17), 1);
 	if (!input[1])
 		input[1] = home;
-	env_edit(env, "OLDPWD", current_dir);
+	env_edit(&msh->envp, "OLDPWD", current_dir);
 	if (chdir(input[1]) == 0)
 	{
 		getcwd(current_dir, PATH_MAX);
-		env_edit(env, "PWD", current_dir);
+		env_edit(&msh->envp, "PWD", current_dir);
 		return (free(home), 0);
 	}
 	else
