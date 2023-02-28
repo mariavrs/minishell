d5c721e14d6d5da032661739dc8e0ddc166fd33a
Maria Vorslova <mariavorslova@gmail.com>
2023-02-28 18:20:52 +0100

    Revert "trash"
    This reverts commit 596b2bbe7d2fc6881ce59f418cf0215300a64ea3.

--- src/ft_free.c
+++ src/ft_free.c
@@ -1,44 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_free.c                                          :+:      :+:    :+:   */
+/*   spl_cmd_malloc_free.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mvorslov <mvorslov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 17:27:19 by mvorslov          #+#    #+#             */
-/*   Updated: 2023/02/28 18:18:33 by mvorslov         ###   ########.fr       */
+/*   Updated: 2023/02/27 02:21:03 by mvorslov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/mini_fun.h"
 
-void	ft_free_str(char *str)
-{
-	if (!str)
-		return ;
-	free(str);
-	str = NULL;
-}
-
-void	ft_free_dbl_str(char **str)
+void	ft_free_dbl_ptr(char **ptr)
 {
 	int	i;
 
 	i = 0;
-	if (!str)
+	if (!ptr)
 		return ;
-	while (str[i])
+	while (ptr[i])
 	{
-		free(str[i]);
-		str[i] = NULL;
+		free(ptr[i]);
+		ptr[i] = NULL;
 		i++;
 	}
-	free(str);
-	str = NULL;
+	free(ptr);
+	ptr = NULL;
 }
 
 void	ft_free_spl_cmd(t_msh *msh)
 {
-	ft_free_str(msh->spl_cmd);
-	ft_free_dbl_str(msh->argv);
+	if (msh->spl_cmd)
+	{
+		free(msh->spl_cmd);
+		msh->spl_cmd = NULL;
+	}
+	free(msh->argv);
 }
--- src/spl_cmd_local_var.c
+++ src/spl_cmd_local_var.c
@@ -6,7 +6,7 @@
 /*   By: mvorslov <mvorslov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 13:02:11 by mvorslov          #+#    #+#             */
-/*   Updated: 2023/02/28 15:13:54 by mvorslov         ###   ########.fr       */
+/*   Updated: 2023/02/28 14:18:38 by mvorslov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ typedef struct s_env_lcl_detect
 int	env_lcl_replace(char *line, t_envl envl, t_msh *msh)
 {
 	if (msh->envp_lcl[envl.env_i])
-		ft_free_str(msh->envp_lcl[envl.env_i]);
+		free(msh->envp_lcl[envl.env_i]);
 	line[envl.ln] = '\0';
 	msh->envp_lcl[envl.env_i] = ft_strjoin(line, envl.env_value);
 	if (!msh->envp_lcl[envl.env_i])
@@ -44,26 +44,28 @@ int	env_lcl_add(char *line, t_envl envl, t_msh *msh)
 	envl.env_i = -1;
 	while (msh->envp_lcl[++envl.env_i])
 		envp_tmp[envl.env_i] = msh->envp_lcl[envl.env_i];
-	ft_free_dbl_str(msh->envp_lcl);
+	free (msh->envp_lcl);
 	msh->envp_lcl = envp_tmp;
 	msh->envp_lcl[envl.env_i] = NULL;
 	msh->envp_lcl[envl.env_i + 1] = NULL;
 	return (env_lcl_replace(line, envl, msh));
 }
 
-char	*get_var_value(char *cursor)
+char	*get_var_value(char *cursor, int *quo_detected)
 {
 	char	*str;
 	int		symb_count;
 
 	str = NULL;
-	symb_count = wrd_collect(cursor);
+	symb_count = wrd_collect(cursor, quo_detected);
 	str = malloc(sizeof(char) * (symb_count + 1));
 	if (!str)
 		return (ft_putstr_fd("minishell: malloc error\n", 2), NULL);
 	str[symb_count] = '\0';
+	if (*quo_detected)
+		cursor++;
 	while (--symb_count >= 0)
-		str[symb_count] = cursor[symb_count];
+		str[symb_count] = *(cursor + symb_count);
 	return (str);
 }
 
@@ -83,25 +85,27 @@ int	find_in_envp_lcl(char *line, int ln, t_msh *msh)
 int	first_wrd_check(int *skip, char *line, t_msh *msh)
 {
 	t_envl	envl;
+	int		quo_detected;
 
 	envl.ln = -1;
+	quo_detected = 0;
 	while (is_valid_varname(line[++envl.ln]) || line[envl.ln] == '=')
 	{
 		if (line[envl.ln] == '=')
 		{
-			envl.env_value = get_var_value(&line[++envl.ln]);
+			envl.env_value = get_var_value(&line[++envl.ln], &quo_detected);
 			if (!envl.env_value)
 				return (*skip = 0, 1);
 			envl.env_vlen = ft_strlen(envl.env_value);
-			*skip = envl.ln + envl.env_vlen;
+			*skip = envl.ln + envl.env_vlen + (quo_detected * 2);
 			if (line[*skip])
-				return (ft_free_str(envl.env_value), 0);
+				return (free(envl.env_value), 0);
 			envl.env_i = find_in_envp_lcl(line, envl.ln, msh);
 			if (envl.env_i >= 0 && env_lcl_replace(line, envl, msh))
-				return (ft_free_str(envl.env_value), *skip = 0, 1);
+				return (free(envl.env_value), *skip = 0, 1);
 			else if (envl.env_i < 0 && env_lcl_add(line, envl, msh))
-				return (ft_free_str(envl.env_value), *skip = 0, 1);
-			return (ft_free_str(envl.env_value), 0);
+				return (free(envl.env_value), *skip = 0, 1);
+			return (free(envl.env_value), 0);
 		}
 	}
 	return (0);
