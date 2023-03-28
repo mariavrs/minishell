97f0e72edd83dc58bb45c42d8622fa5f8d46cde8
DE SMET Erick <erickdesmet@gmail.com>
2023-03-28 21:55:29 +0200

    echo fix: - echo -nn bonjour 	-nn should be like -n - echo -n -n -n bonjour 	-n -n -n should be like -n
    cd fix:
    - if folder doesnt exist should print like an error
    	cd: /var/file_tests: No such file or directory
    - cd can’t manage few arguments
    	cd: too many arguments

--- src/builtins/ft_cd.c
+++ src/builtins/ft_cd.c
@@ -6,24 +6,33 @@
 /*   By: ede-smet <ede-smet@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 16:54:40 by ede-smet          #+#    #+#             */
-/*   Updated: 2023/03/19 14:41:18 by ede-smet         ###   ########.fr       */
+/*   Updated: 2023/03/28 21:53:35 by ede-smet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/mini_fun.h"
+
+static int	cd_error(char **input, char *home)
+{
+	if (!input)
+		return (1);
+	if (env_size(input) > 2)
+		return (write(2, "cd: too many arguments\n", 23), 1);
+	if (!home && !input[1])
+		return (write(2, "cd: HOME not set\n", 17), 1);
+	return (0);
+}
 
 int	ft_cd(char **input, t_msh *msh)
 {
 	char	current_dir[PATH_MAX];
 	char	*dir;
 	char	*home;
+	char	*error;
 
-	if (!input)
-		return (1);
-	getcwd(current_dir, PATH_MAX);
 	home = env_get(msh->envp, "HOME");
-	if (!home && !input[1])
-		return (write(2, "cd: HOME not set\n", 17), 1);
+	if (getcwd(current_dir, PATH_MAX) && cd_error(input, home))
+		return (ft_free_str(&home), 1);
 	if (input[1])
 		dir = input[1];
 	else
@@ -37,5 +46,8 @@ int	ft_cd(char **input, t_msh *msh)
 	else if (env_edit(&msh->envp, "OLDPWD", current_dir))
 		return (ft_putstr_fd("minishell: malloc error\n", 2), 1);
 	else
-		return (perror("cd"), ft_free_str(&home), 1);
+	{
+		error = ft_strjoin("cd: ", input[1]);
+		return (perror(error), ft_free_str(&error), ft_free_str(&home), 1);
+	}
 }
