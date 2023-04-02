9e4035919b92e5dfc034ab8770333e0a5c5ac20b
DE SMET Erick <erickdesmet@gmail.com>
2023-04-02 18:01:00 +0200

    if the argv[i] is \0 it will not print the space char

--- src/builtins/ft_echo.c
+++ src/builtins/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: ede-smet <ede-smet@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 16:54:40 by ede-smet          #+#    #+#             */
-/*   Updated: 2023/03/28 21:33:18 by ede-smet         ###   ########.fr       */
+/*   Updated: 2023/04/02 17:21:15 by ede-smet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 static void	ft_print_line(char **input, int i)
 {
 	ft_putstr_fd(input[i], 1);
-	if (input[i + 1])
+	if (input[i][0] != '\0' && input[i + 1] && input[i + 1][0] != '\0')
 		ft_putstr_fd(" ", 1);
 }
 
