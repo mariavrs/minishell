879af2c834ded44064fe11a84d16baa09509662f
Maria Vorslova VM <mariavorslova@gmail.com>
2023-04-02 16:07:01 +0000

    get full var to add/replace in env fixed to fit expansion case

--- src/env_declare_get_full_var.c
+++ src/env_declare_get_full_var.c
@@ -6,7 +6,7 @@
 /*   By: mvorslov <mvorslov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 17:17:30 by mvorslov          #+#    #+#             */
-/*   Updated: 2023/04/02 11:38:16 by mvorslov         ###   ########.fr       */
+/*   Updated: 2023/04/02 15:57:56 by mvorslov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,18 @@
 int	get_env_mod(char c)
 {
 	if (c == '=')
-		return (ENV_CREATE);
-	else
+		return (ENV_REPLC);
+	else if (c == '+')
 		return (ENV_APPEND);
+	else
+		return (ENV_EXPND);
 }
 
 static void	full_var_join(t_env *env, t_msh *msh)
 {
 	int	len_if_in_envp;
 
-	if (env->mod == ENV_CREATE)
+	if (env->mod == ENV_REPLC)
 	{
 		ft_strlcpy(env->full_var, env->name, env->name_ln + 1);
 		env->full_var[env->name_ln] = '=';
@@ -42,6 +44,8 @@ static void	full_var_join(t_env *env, t_msh *msh)
 	{
 		len_if_in_envp = ft_strlen(msh->envp[env->i]);
 		ft_strlcpy(env->full_var, msh->envp[env->i], len_if_in_envp + 1);
+		if (!msh->envp[env->name_ln])
+			env->full_var[len_if_in_envp++] = '=';
 		ft_strlcpy(&env->full_var[len_if_in_envp],
 			env->value, env->value_ln + 1);
 	}
@@ -72,19 +76,34 @@ int	find_in_envp(t_env *env, t_msh *msh)
 	return (env->src = -1, -1);
 }
 
+int	full_var_if_expand(t_env *env, t_msh *msh)
+{
+	if (env->src == ENV_LCL)
+		env->full_var = ft_strdup(msh->envp_lcl[env->i]);
+	else if (env->src == ENV_EXP)
+		env->full_var = ft_strdup(msh->envp[env->i]);
+	else
+		env->full_var = ft_strdup(env->name);
+	if (!env->full_var)
+		return (ft_putstr_fd("minishell: malloc error\n", 2), 1);
+	return (0);
+}
+
 int	get_full_var_str(char *line, t_env *env, t_msh *msh)
 {
 	env->name = line;
 	env->i = find_in_envp(env, msh);
 	env->value_ln = ft_strlen(env->value);
 	if (env->mod == ENV_APPEND && env->i < 0)
-		env->mod = ENV_CREATE;
-	if (env->mod == ENV_CREATE)
+		env->mod = ENV_REPLC;
+	if (env->mod == ENV_REPLC)
 		env->full_var_ln = env->name_ln + env->value_ln + 1;
 	else if (env->mod == ENV_APPEND && env->src == ENV_LCL)
 		env->full_var_ln = ft_strlen(msh->envp_lcl[env->i]) + env->value_ln;
 	else if (env->mod == ENV_APPEND && env->src == ENV_EXP)
-		env->full_var_ln = ft_strlen(msh->envp[env->i]) + env->value_ln;
+		env->full_var_ln = ft_strlen(msh->envp[env->i]) + 1 + env->value_ln;
+	if (env->mod == ENV_EXPND)
+		return (full_var_if_expand(env, msh));
 	env->full_var = ft_malloc_str(env->full_var_ln + 1);
 	if (!env->full_var)
 		return (1);
