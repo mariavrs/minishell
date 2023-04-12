dc6e212c8e4c42f06120c4c799cc64672ac4909e
Maria Vorslova <mariavorslova@gmail.com>
2023-04-13 01:58:34 +0200

    stdio_backup.c + little improvement in ft_exit_error

--- src/spl_cmd_redir_exec.c
+++ src/spl_cmd_redir_exec.c
@@ -6,7 +6,7 @@
 /*   By: mvorslov <mvorslov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 03:31:33 by mvorslov          #+#    #+#             */
-/*   Updated: 2023/04/12 19:29:18 by mvorslov         ###   ########.fr       */
+/*   Updated: 2023/04/13 01:53:44 by mvorslov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,59 +63,4 @@ int	run_redir(t_msh *msh, t_cmd *cmd)
 	}
 	ft_free_redir_elem(&cmd->rdr);
 	return (run_redir(msh, cmd));
-}
-
-int	get_backup_stdio(t_msh *msh, t_cmd *cmd)
-{
-	if (msh->pipe_flag)
-	{
-		cmd->rdr_in_flag = 0;
-		cmd->rdr_out_flag = 0;
-	}
-	if (cmd->rdr_in_flag)
-	{
-		cmd->stdin_backup = dup(STDIN_FILENO);
-		if (cmd->stdin_backup == -1)
-			return (perror("minishell"), ft_putendl_fd("I/O backup failed", 2),
-				ft_putendl_fd("exit", 1), 1);
-	}
-	if (cmd->rdr_out_flag)
-	{
-		cmd->stdout_backup = dup(STDOUT_FILENO);
-		if (cmd->stdout_backup == -1)
-		{
-			if (cmd->stdin_backup >= 0)
-				close(cmd->stdin_backup);
-			return (perror("minishell"), ft_putendl_fd("I/O backup failed", 2),
-				ft_putendl_fd("exit", 1), 1);
-		}
-	}
-	return (0);
-}
-
-void	put_backup_stdio(t_msh *msh, t_cmd *cmd)
-{
-	if (cmd->rdr_in_flag)
-	{
-		if (dup2(cmd->stdin_backup, STDIN_FILENO) == -1)
-		{
-			g_exit_status = ERR_IO;
-			perror("minishell");
-			close(STDIN_FILENO);
-		}
-		close(cmd->stdin_backup);
-	}
-	if (cmd->rdr_out_flag)
-	{
-		if (dup2(cmd->stdout_backup, STDOUT_FILENO) == -1)
-		{
-			g_exit_status = ERR_IO;
-			perror("minishell");
-			close(STDOUT_FILENO);
-		}
-		close(cmd->stdout_backup);
-	}
-	if ((cmd->rdr_in_flag || cmd->rdr_out_flag) && g_exit_status == ERR_IO)
-		return (ft_putendl_fd("STD I/O could not be restored", 2),
-			ft_putendl_fd("exit", 1), ft_free_exit(msh), exit(ERR_IO));
 }
---
+++ src/stdio_backup.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stdio_backup.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvorslov <mvorslov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/13 01:53:16 by mvorslov          #+#    #+#             */
+/*   Updated: 2023/04/13 01:55:23 by mvorslov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/mini_fun.h"
+
+extern int	g_exit_status;
+
+int	get_backup_stdio(t_msh *msh, t_cmd *cmd)
+{
+	if (msh->pipe_flag)
+	{
+		cmd->rdr_in_flag = 0;
+		cmd->rdr_out_flag = 0;
+	}
+	if (cmd->rdr_in_flag)
+	{
+		cmd->stdin_backup = dup(STDIN_FILENO);
+		if (cmd->stdin_backup == -1)
+			return (perror("minishell"), ft_putendl_fd("I/O backup failed", 2),
+				ft_putendl_fd("exit", 1), 1);
+	}
+	if (cmd->rdr_out_flag)
+	{
+		cmd->stdout_backup = dup(STDOUT_FILENO);
+		if (cmd->stdout_backup == -1)
+		{
+			if (cmd->stdin_backup >= 0)
+				close(cmd->stdin_backup);
+			return (perror("minishell"), ft_putendl_fd("I/O backup failed", 2),
+				ft_putendl_fd("exit", 1), 1);
+		}
+	}
+	return (0);
+}
+
+static int	backup_dup_back(int fd_backup, int fd_stdio, int io_flag)
+{
+	int	status;
+
+	status = dup2(fd_backup, fd_stdio);
+	close(fd_backup);
+	if (status != -1)
+		return (0);
+	g_exit_status = ERR_IO;
+	ft_putstr_fd("minishell: ", 2);
+	if (!io_flag)
+		perror("STDIN");
+	else if (io_flag == 1)
+		perror("STDOUT");
+	close(fd_stdio);
+	return (1);
+}
+
+void	put_backup_stdio(t_msh *msh, t_cmd *cmd)
+{
+	int	status;
+
+	status = 0;
+	if (msh->pipe_flag)
+		return ;
+	if (cmd->rdr_in_flag)
+		status += backup_dup_back(cmd->stdin_backup, STDIN_FILENO, 0);
+	if (cmd->rdr_out_flag)
+		status += backup_dup_back(cmd->stdout_backup, STDOUT_FILENO, 1);
+	if (status > 0)
+	{
+		ft_putendl_fd("STD I/O could not be restored", 2);
+		ft_exit_error(0, NULL, msh, ERR_IO);
+	}
+}
