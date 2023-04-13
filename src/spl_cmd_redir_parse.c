bdeddbce2509b1413c3c65621020960b8ffe16c5
Maria Vorslova <mariavorslova@gmail.com>
2023-04-13 03:14:51 +0200

    norminette

--- src/spl_cmd_redir_parse.c
+++ src/spl_cmd_redir_parse.c
@@ -6,7 +6,7 @@
 /*   By: mvorslov <mvorslov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 03:00:07 by mvorslov          #+#    #+#             */
-/*   Updated: 2023/04/11 16:22:42 by mvorslov         ###   ########.fr       */
+/*   Updated: 2023/04/13 03:14:24 by mvorslov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,21 +27,28 @@ static int	get_len_in_cmd(char *line)
 	return (len);
 }
 
+static char	*get_filename_raw(t_msh *msh, t_cmd *cmd, int *i, int cursor)
+{
+	char	*filename_raw;
+	char	tmp;
+
+	tmp = cmd->spl_cmd[*i];
+	cmd->spl_cmd[*i] = '\0';
+	filename_raw = param_expansion(&cmd->spl_cmd[cursor], msh,
+			quo_check(cmd->spl_cmd[cursor], 0), 0);
+	cmd->spl_cmd[*i] = tmp;
+	return (filename_raw);
+}
+
 static char	*get_redir_filename(t_msh *msh, t_cmd *cmd, int *i)
 {
 	char	*filename_raw;
 	char	*filename;
-	char	tmp;
 	int		cursor;
 
 	cursor = *i;
 	*i += get_len_in_cmd(&cmd->spl_cmd[*i]);
-	tmp = cmd->spl_cmd[*i];
-	cmd->spl_cmd[*i] = '\0';
-	filename_raw = NULL;
-	filename_raw = param_expansion(&cmd->spl_cmd[cursor], msh,
-			quo_check(cmd->spl_cmd[cursor], 0), 0);
-	cmd->spl_cmd[*i] = tmp;
+	filename_raw = get_filename_raw(msh, cmd, i, cursor);
 	if (!filename_raw)
 		return (cmd->parse_status = 1, NULL);
 	while (cursor < *i)
@@ -61,22 +68,21 @@ static char	*get_redir_filename(t_msh *msh, t_cmd *cmd, int *i)
 	return (filename);
 }
 
-static char	get_redir_mode(char *spl_cmd, int i)
+static char	get_redir_mode(t_cmd *cmd, int i)
 {
-	if (spl_cmd[i] == '>' && spl_cmd[i + 1] == '>')
-		return ('+');
-	else if (spl_cmd[i] == '<' && spl_cmd[i + 1] == '<')
-		return ('-');
-	else
-		return (spl_cmd[i]);
-}
-
-static void	set_mode_flag(t_cmd *cmd, char mode)
-{
-	if (mode == '-' || mode == '<')
-		cmd->rdr_in_flag = 1;
-	if (mode == '+' || mode == '>')
+	if (cmd->spl_cmd[i] == '>')
+	{
 		cmd->rdr_out_flag = 1;
+		if (cmd->spl_cmd[i + 1] == '>')
+			return ('+');
+	}
+	else if (cmd->spl_cmd[i] == '<')
+	{
+		cmd->rdr_in_flag = 1;
+		if (cmd->spl_cmd[i + 1] == '<')
+			return ('-');
+	}
+	return (cmd->spl_cmd[i]);
 }
 
 t_redir	*parse_redir(t_msh *msh, t_cmd *cmd, int i, int quo_flag)
@@ -92,7 +98,7 @@ t_redir	*parse_redir(t_msh *msh, t_cmd *cmd, int i, int quo_flag)
 	rdr = malloc(sizeof(t_redir));
 	if (!rdr)
 		return (malloc_error(), cmd->parse_status = 1, NULL);
-	rdr->mode = get_redir_mode(cmd->spl_cmd, i);
+	rdr->mode = get_redir_mode(cmd, i);
 	while (cmd->spl_cmd[i] == '<' || cmd->spl_cmd[i] == '>')
 		cmd->spl_cmd[i++] = ' ';
 	while (is_in_str(cmd->spl_cmd[i], STR_WHSPACE))
@@ -104,6 +110,5 @@ t_redir	*parse_redir(t_msh *msh, t_cmd *cmd, int i, int quo_flag)
 		cmd->parse_status = redir_heredoc(msh, rdr);
 	if (cmd->parse_status)
 		return (ft_free_redir(&rdr), NULL);
-	set_mode_flag(cmd, rdr->mode);
 	return (rdr->next = parse_redir(msh, cmd, i, quo_flag), rdr);
 }
